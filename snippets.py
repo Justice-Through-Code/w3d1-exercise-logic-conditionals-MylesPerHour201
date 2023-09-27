e853df0bee4174bff74093e8c4d9e8874c0421d8
MylesPerHour201 <beck.mj1993@gmail.com>
2023-09-26 21:23:53 -0400

    changed line41 print statement

--- snippets.py
+++ snippets.py
@@ -38,7 +38,7 @@ def snippet_3():
     b = 1
     c = (a >= b)
 
-    print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
+    print(f"The value of c ({c}) is True since a ({a}) is greater than or equal to b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
 
 
